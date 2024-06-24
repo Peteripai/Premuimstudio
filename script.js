923d2f57642102caf5ef658e38a50ec96e5568bd
PETERIPAII.C <107237414+Peteripai@users.noreply.github.com>
2024-06-24 10:24:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,24 @@ nextBtn.addEventListener('click', function() {
     const scrollAmount = galleryItems[currentIndex].offsetWidth;
     gallery.scrollLeft += scrollAmount;
 });
+    document.addEventListener("DOMContentLoaded", function() {
+            var backToTop = document.getElementById("back-to-top");
 
+            // Show or hide the button
+            window.onscroll = function() {
+                if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                    backToTop.style.display = "block";
+                } else {
+                    backToTop.style.display = "none";
+                }
+            };
+
+            // Scroll to the top smoothly
+            backToTop.addEventListener("click", function(event) {
+                event.preventDefault();
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                });
+            });
+        });
